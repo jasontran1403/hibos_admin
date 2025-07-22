2939a5d7d293f98d0b9b7999516772de352242d1
jasontran1403 <hainguyentran14@gmail.com>
2025-07-22 18:28:23 +0700

    update endpint

--- src/types/constant.js
+++ src/types/constant.js
@@ -1,3 +1,3 @@
 // export const URL = "http://localhost:8889/api/v1/";
-export const URL = "https://sure-innocent-mosquito.ngrok-free.app/api/v1/";
+export const URL = "https://tidy-credible-quail.ngrok-free.app/api/v1/";
 
